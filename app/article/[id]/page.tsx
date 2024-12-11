efd23ec331fa5a3fbb40183e1d3a59f3a86c61d0
Robert Alexander <gogonegro@gmail.com>
2024-12-11 19:56:43 +0100

    refactor: Simplify JSX structure for meta data grid

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -227,8 +227,7 @@ export default function ArticlePage({ params }: PageProps) {
 
 
         {article.meta_data ? (
-          <>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {filterEntities().map((entity) => (
               <Card
                 key={entity.id}
