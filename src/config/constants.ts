450ef58773183ea679fc628d1af20776192800de
Robert Alexander <gogonegro@gmail.com>
2024-12-29 18:33:50 +0100

    prettier fix

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,9 @@
 const SPARQL_ENDPOINT = process.env.NEXT_PUBLIC_SPARQL_ENDPOINT;
 
 if (!SPARQL_ENDPOINT) {
-  throw new Error("NEXT_PUBLIC_SPARQL_ENDPOINT environment variable is not set in a .env file");
+  throw new Error(
+    "NEXT_PUBLIC_SPARQL_ENDPOINT environment variable is not set in a .env file",
+  );
 }
 
 export const ENDPOINTS = {
